62a8f960d5e833e4b9db591ca8ea44bd160f1f95
RekaSzuromi <szuromir2@gmail.com>
2024-01-17 01:46:00 +0100

    Upload csv framework

--- pricelab/experiments/admin.py
+++ pricelab/experiments/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django import forms
 
 from .models import Experiment
 from .models import User
+
+class CsvImportForm(forms.Form):
+    csv_upload = forms.FileField()
 
 @admin.register(Experiment)
 class ExperimentAdmin(admin.ModelAdmin):
@@ -10,3 +16,16 @@ class ExperimentAdmin(admin.ModelAdmin):
 @admin.register(User)
 class UsersAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'location', 'age', 'avg_minutes_per_ride']
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv),]
+        return new_urls + urls
+    
+    def upload_csv(self, request):
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_upload.html", data)
+    
+
+
