43a70bd6f4ae588d043049184177559eb556ad86
Barager <70955623+Barager@users.noreply.github.com>
2024-01-22 11:20:16 +0100

    added code for tests

--- pricelab/experiments/models.py
+++ pricelab/experiments/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from datetime import timedelta
 from datetime import datetime
 import random
+import pandas as pd
+import numpy as np  
+
+from sklearn.model_selection import StratifiedShuffleSplit
 
 class Experiment(models.Model):
     CRITERIA_AND_FILTER_CHOICES = [('location','Location'), ('age', 'Age'), ('avg_minutes_per_ride', 'Minutes Per Ride')]
@@ -19,6 +23,7 @@ class Experiment(models.Model):
     treatment_group_ratio = models.PositiveIntegerField(default=50, help_text='Percentage of users to receive treatment')
     treatment_group = models.ManyToManyField('User', related_name='treatment_group', blank=True)
     control_group = models.ManyToManyField('User', related_name='control_group', blank=True)
+    experiment_ready = models.BooleanField(default=False)
 
     def calculate_percentage(self):
         current_date = timezone.now()
@@ -47,13 +52,18 @@ class Experiment(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         treatment_group_ratio = self.treatment_group_ratio
-        users = User.objects.all()
-        user_ids = [user.id for user in users]
-        random.shuffle(user_ids)
-        num_users = len(user_ids)
-        num_treatment_group = int(num_users * treatment_group_ratio / 100)
-        self.treatment_group.set(user_ids[:num_treatment_group])
-        self.control_group.set(user_ids[num_treatment_group:])
+
+        users = User.objects.values('id', 'location_title', 'rides')
+        df = pd.DataFrame(users)
+
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=(1 - treatment_group_ratio / 100), random_state=42)
+
+        for train_idx, test_idx in sss.split(df, df[['location_title']]):
+            
+            treatment_ids = df.loc[train_idx, 'id'].tolist()
+            control_ids = df.loc[test_idx, 'id'].tolist()
+            self.treatment_group.set(treatment_ids)
+            self.control_group.set(control_ids)
     
     
 class User(models.Model):
